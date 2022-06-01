2d45aab9c3059500bcdfa71486b308b8922ac64a
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-06-01 10:47:29 +0300

    kj;ho

--- src/ fetchPhoto.js
+++ src/ fetchPhoto.js
@@ -1,12 +1,31 @@
-export  function fetchPhoto(name){
-        return  fetch(`https://pixabay.com/api/?key=27737984-087ff865c77ff0cde11c21156&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`)
+export default class ApiPhoto{
+    constructor(){
+this.searchText=``;
+this.page=1;
+    }
+     fetchPhoto(){
+       
+         return fetch(`https://pixabay.com/api/?key=27737984-087ff865c77ff0cde11c21156&q=${this.searchText}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
           .then(r =>{
               if(r.ok){
-               return r.json()  
+             return r.json()  
               } else {
                   throw new Error(r.message);
               }
               
-          } )
+          } ).then(data=>{
+              this.page+=1;
+              return data
+            })
       }
+      get query (){
+          return this.searchText;
+      }
+      set query (newSearchText){
+          return this.searchText = newSearchText;
+      }
+      resetPage(){
+        this.page=1;
+      }
+}
 
--- src/index.js
+++ src/index.js
@@ -1,53 +1,118 @@
-import {fetchPhoto} from "./ fetchPhoto";
+import ApiPhoto from "./ fetchPhoto";
+import Notiflix from 'notiflix';
 
 const form = document.querySelector(`form`);
 const divGallary = document.querySelector(`.gallery`);
 const loadMore = document.querySelector(`.load-more`);
+const Photo = new ApiPhoto();
+const hOne = document.querySelector(`h1`);
+console.log(hOne)
 
-let searchText = ``;
+function deleteBtn(){
+  loadMore.classList.add(`is-hidden`)
+}
+function recoveryBtn(){
+  loadMore.classList.remove(`is-hidden`)
+}
+
+function clearContent (){
+  divGallary.innerHTML=``;
+}
+
+deleteBtn();
+
 form.addEventListener(`submit`,(e)=>{
     e.preventDefault();
-     searchText = e.currentTarget.searchQuery.value.trim();
-if(searchText===``){
-    return
-}
 
-fetchPhoto(searchText).then(photographs =>{
-    console.log(photographs.hits);
-  const htmlMarkup = photographs.hits.map(p=>{
-    console.log(p);
-      return  `<div class="photo-card">
-        <img src="${p.webformatURL}" alt="${p.tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-            ${p.likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-            ${p.views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-            ${p.comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-            ${p.downloads}
-          </p>
-        </div>
-      </div>`
-    }).join(``);
-    divGallary.insertAdjacentHTML(`afterbegin`, htmlMarkup);
-    
-}
-)
+    recoveryBtn();
+    clearContent ();
+     Photo.query = e.currentTarget.searchQuery.value.trim();
+     if(Photo.searchText==``){
+      Notiflix.Notify.info('Видите запрос');
+      return;
+    }
+     Photo.resetPage();
+     Photo.fetchPhoto()
 
-// .then(console.log)
-// .then(photo => console.log(photo))
-// .catch(
-//     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-// )
+     .then(photographs =>{
+          console.log(photographs);
+          if(photographs.hits.length == 0){
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            
+          }
+        const htmlMarkup = photographs.hits.map(p=>{
+          console.log(p);
+            return  `<div class="photo-card ">
+              <img src="${p.webformatURL}" alt="${p.tags}" loading="lazy" />
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes</b>
+                  ${p.likes}
+                </p>
+                <p class="info-item">
+                  <b>Views</b>
+                  ${p.views}
+                </p>
+                <p class="info-item">
+                  <b>Comments</b>
+                  ${p.comments}
+                </p>
+                <p class="info-item">
+                  <b>Downloads</b>
+                  ${p.downloads}
+                </p>
+              </div>
+            </div>`
+          }).join(``);
+          divGallary.insertAdjacentHTML(`afterbegin`, htmlMarkup);
+       })
+
 } )
+
+loadMore.addEventListener(`click`,()=>{
+
+  deleteBtn()
+  Photo.fetchPhoto()
+  
+  .then(photographs =>{
+        console.log(photographs.hits);
+      const htmlMarkup = photographs.hits.map(p=>{
+        console.log(p);
+          return  `<div class="photo-card ">
+            <img src="${p.webformatURL}" alt="${p.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${p.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${p.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${p.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>
+                ${p.downloads}
+              </p>
+            </div>
+          </div>`
+        }).join(``);
+        recoveryBtn();
+        divGallary.insertAdjacentHTML('beforeend', htmlMarkup);
+        console.log(photographs);
+        if(photographs.hits.length === 0){
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          deleteBtn();
+          hOne.innerHTML=`We're sorry, but you've reached the end of search results.`
+          }
+})
+
+
+}) 
+
+
 
 
